400ddd998d4864c5f75f7d3be6d793e4fd5d7fb9
Nelson Campos <nelsonramirocampos@gmail.com>
2021-05-21 21:40:54 -0300

    Se inicia el proyecto.

---
+++ TrabajoPracticoN5/TrabajoPracticoN5/Simulacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPracticoN5
+{
+    public partial class Simulacion : Form
+    {
+        public Simulacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
